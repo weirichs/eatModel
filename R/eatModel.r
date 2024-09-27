975e4ec28b296201602c8f8eb3c55f47ef03f476
Edna Gwendolyn Daphne Grewers <grewered@user.hu-berlin.de>
2024-09-27 12:30:45 +0200

    organized `prepRep`

--- R/eatModel.r
+++ R/eatModel.r
@@ -82,42 +82,68 @@ getConquestVersion <- function(path.conquest, path.temp, asDate = TRUE){
 
 ### not called -----------------------------------------------------------------
 
-prepRep <- function ( calibT2, bistaTransfT1, bistaTransfT2, makeIdsUnique = TRUE) {
-           if ( !inherits(calibT2, "transfBista" )) { stop("'calibT2' object must be of class 'transfBista'.\n")}
-           if ( !inherits(bistaTransfT1, "transfBista" )) { stop("'bistaTransfT2' object must be of class 'transfBista'.\n")}
-           if ( !inherits(bistaTransfT2, "transfBista") ) { stop("'bistaTransfT2' object must be of class 'transfBista'.\n")}
-           if (!nrow(calibT2[["itempars"]]) < nrow(bistaTransfT1[["itempars"]])) { stop("Mismatch between 'calibT2' and 'bistaTransfT1'. \n")}
-           if (!nrow(calibT2[["itempars"]]) < nrow(bistaTransfT2[["itempars"]])) { stop("Mismatch between 'calibT2' and 'bistaTransfT2'. \n")}
-           idT1<- unique(bistaTransfT1[["all.Names"]][which(bistaTransfT1[["all.Names"]][,"par"] == "ID"),"derived.par"])
-           idT2<- unique(bistaTransfT2[["all.Names"]][which(bistaTransfT2[["all.Names"]][,"par"] == "ID"),"derived.par"])
-           stopifnot(length(idT1)==1, length(idT2)==1)
-           if ( idT1 != idT2 ) {
-                warning(paste0("ID variables do not match between t1 and t2. ID for t1: '",idT1,"'. ID for t2: '",idT2,"'. \n    IDs will be unified with '",idT1,"'."))
-                recStat <- paste ( "'", idT2 , "' = '", idT1, "'", sep="")
-                colnames ( bistaTransfT2[["personpars"]] ) <- car::recode ( colnames ( bistaTransfT2[["personpars"]] ), recStat)
-           }
-           lc  <- colnames( calibT2[["personpars"]] ) [grep("^linking", colnames(calibT2[["personpars"]]) )]
-           if(length(lc)==0) { stop("No columns with linking error information found in 'calibT2'.\n")}
-           lcn <- paste("trend", eatTools::removePattern(string = lc, pattern = "linking"), sep="")
-           colnames( calibT2[["personpars"]] ) [grep("^linking", colnames(calibT2[["personpars"]]) )] <- lcn
-           merg<- c("group", "imp", "traitLevel", "dimension")
-           frms<- list ( calibT2=calibT2, bistaTransfT1=bistaTransfT1, bistaTransfT2=bistaTransfT2 )
-           toM <- unique(unlist(lapply ( names(frms), FUN = function ( l.Name ) {
-                  l    <- frms[[l.Name]]
-                  drin <- merg %in% colnames(l[["personpars"]])
-                  fehlt<- merg[which(drin==FALSE)]
-                  if (!all(drin == TRUE)) { warning(paste0("Column(s) '",paste(fehlt, collapse = "', '"), "' are unexpectedly missing in '",l.Name,"'."))}
-                  keep <- merg[which(drin==TRUE)]
-                  return(keep)})))
-           if(length(toM)==0) { stop("Merging impossible.\n")}
-           red <- calibT2[["personpars"]][,c(toM,  lcn)]
-           red <- red[!duplicated(red),]
-           dat1<- data.frame ( trend = "T1" , merge ( bistaTransfT1[["personpars"]], red, by = toM, all = TRUE))
-           stopifnot ( nrow(dat1) == nrow(bistaTransfT1[["personpars"]]))
-           dat2<- data.frame ( trend = "T2" , merge ( bistaTransfT2[["personpars"]], red, by = toM, all = TRUE))
-           stopifnot ( nrow(dat2) == nrow(bistaTransfT2[["personpars"]]))
-           if ( makeIdsUnique == TRUE ) {
-                dat1[, paste(idT1, "unique", sep="_")] <- paste(dat1[, "trend"], dat1[, idT1], sep="_")
-                dat2[, paste(idT1, "unique", sep="_")] <- paste(dat2[, "trend"], dat2[, idT1], sep="_")
-           }
-           return(rbind ( dat1, dat2))}
+prepRep <- function(calibT2, bistaTransfT1, bistaTransfT2, makeIdsUnique = TRUE){
+  # checks
+  if(!inherits(calibT2, "transfBista")){
+    stop("'calibT2' object must be of class 'transfBista'.\n")
+  }
+  if(!inherits(bistaTransfT1, "transfBista" )){
+    stop("'bistaTransfT2' object must be of class 'transfBista'.\n")
+  }
+  if(!inherits(bistaTransfT2, "transfBista")){
+    stop("'bistaTransfT2' object must be of class 'transfBista'.\n")
+  }
+  if(!nrow(calibT2[["itempars"]]) < nrow(bistaTransfT1[["itempars"]])){
+    stop("Mismatch between 'calibT2' and 'bistaTransfT1'. \n")
+  }
+  if(!nrow(calibT2[["itempars"]]) < nrow(bistaTransfT2[["itempars"]])){
+    stop("Mismatch between 'calibT2' and 'bistaTransfT2'. \n")
+  }
+
+  # function
+  idT1 <- unique(bistaTransfT1[["all.Names"]][which(bistaTransfT1[["all.Names"]][,"par"] == "ID"),"derived.par"])
+  idT2 <- unique(bistaTransfT2[["all.Names"]][which(bistaTransfT2[["all.Names"]][,"par"] == "ID"),"derived.par"])
+  stopifnot(length(idT1)==1, length(idT2)==1)
+  if(idT1 != idT2){
+    warning(paste0("ID variables do not match between t1 and t2. ID for t1: '",idT1,"'. ID for t2: '",idT2,"'. \n    IDs will be unified with '",idT1,"'."))
+    recStat <- paste ( "'", idT2 , "' = '", idT1, "'", sep="")
+    colnames(bistaTransfT2[["personpars"]]) <- car::recode(colnames(bistaTransfT2[["personpars"]]), recStat)
+  }
+
+  lc <- colnames(calibT2[["personpars"]])[grep("^linking", colnames(calibT2[["personpars"]]))]
+  if(length(lc)==0){
+    stop("No columns with linking error information found in 'calibT2'.\n")
+  }
+  lcn <- paste("trend", eatTools::removePattern(string = lc, pattern = "linking"), sep="")
+  colnames(calibT2[["personpars"]])[grep("^linking", colnames(calibT2[["personpars"]]))] <- lcn
+
+  merg <- c("group", "imp", "traitLevel", "dimension")
+  frms <- list(calibT2=calibT2, bistaTransfT1=bistaTransfT1, bistaTransfT2=bistaTransfT2)
+  toM  <- unique(unlist(lapply(names(frms), FUN = function(l.Name){
+    l     <- frms[[l.Name]]
+    drin  <- merg %in% colnames(l[["personpars"]])
+    fehlt <- merg[which(drin==FALSE)]
+    if (!all(drin == TRUE)){
+      warning(paste0("Column(s) '",paste(fehlt, collapse = "', '"), "' are unexpectedly missing in '",l.Name,"'."))
+    }
+    keep <- merg[which(drin==TRUE)]
+    return(keep)
+  })))
+
+  if(length(toM)==0){
+    stop("Merging impossible.\n")
+  }
+  red <- calibT2[["personpars"]][,c(toM, lcn)]
+  red <- red[!duplicated(red),]
+
+  dat1 <- data.frame(trend = "T1", merge(bistaTransfT1[["personpars"]], red, by = toM, all = TRUE))
+  stopifnot(nrow(dat1) == nrow(bistaTransfT1[["personpars"]]))
+  dat2 <- data.frame(trend = "T2", merge(bistaTransfT2[["personpars"]], red, by = toM, all = TRUE))
+  stopifnot(nrow(dat2) == nrow(bistaTransfT2[["personpars"]]))
+
+  if(makeIdsUnique == TRUE){
+    dat1[, paste(idT1, "unique", sep="_")] <- paste(dat1[, "trend"], dat1[, idT1], sep="_")
+    dat2[, paste(idT1, "unique", sep="_")] <- paste(dat2[, "trend"], dat2[, idT1], sep="_")
+  }
+  return(rbind(dat1, dat2))
+}
