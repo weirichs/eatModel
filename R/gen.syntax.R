3ee8b538cc40c8fe9b185535b84573b74b3a028a
Sebastian <sebastian.weirich@iqb.hu-berlin.de>
2026-04-29 11:17:27 +0200

    defineModel.rd help page
    more streamlined examples that run faster

--- R/gen.syntax.R
+++ R/gen.syntax.R
@@ -34,6 +34,7 @@ gen.syntax     <- function(Name,daten, all.Names, namen.all.hg = NULL, all.hg.ch
    "descriptives !estimates=pv >> ####hier.name.einfuegen####_pvl.dsc;",
    "descriptives !estimates=wle >> ####hier.name.einfuegen####_wle.dsc;",
    "quit;")
+   all.inf   <- Sys.getenv()
    sysInfo   <- Sys.info()
    if(is.null(Title))   {                                       ### wenn kein Titel gesetzt, erstelle ihn aus Sys.getenv()
       if(sysInfo[["sysname"]] == "Linux") {
@@ -43,7 +44,6 @@ gen.syntax     <- function(Name,daten, all.Names, namen.all.hg = NULL, all.hg.ch
          user <- all.inf["USERNAME"]
          comp <- all.inf["COMPUTERNAME"]
       }
-      all.inf  <- Sys.getenv()
       Title    <- paste("Analysis name: ",Name, ", User: ",user,", Computername: ",comp,", ", R.version$version.string , ", Time: ",date(),sep="")
    }
    converge <- paste("0",substring(as.character(converge+1),2),sep="")
